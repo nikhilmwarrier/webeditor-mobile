e243b7901bff2bf495ee75b9ef30870b8fb3c369
Nikhil M Warrier <70085759+nikhilmwarrier@users.noreply.github.com>
2021-07-10 05:10:46 +0000

    Add manifest, icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,13 @@ function Sidebar() {
 			<div className="flex">
 				<h3>Webeditor Mobile</h3>
 				<p>Built and opensourced by nikhilmwarrier</p>
+				<p>
+					Install{" "}
+					<a href="https://play.google.com/store/apps/details?id=org.pocketworkstation.pckeyboard&hl=en_IN&gl=US">
+						Hacker's Keyboard
+					</a>{" "}
+					by Claudius Weiner <br /> for a better experience
+				</p>
 			</div>
 			<a className="repo-link" href="https://github.com/nikhilmwarrier/webeditor-mobile">
 				@nikhilmwarrier/webeditor-mobile
